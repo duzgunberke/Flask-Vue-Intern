e989a389c0bc0f9979c5ecfa38bab9e80820060f
duzgunberke <79326338+duzgunberke@users.noreply.github.com>
2022-11-21 00:33:19 +0300

    db model update

--- src/backend/classes.py
+++ src/backend/classes.py
@@ -1,26 +1,43 @@
 from mongoengine import *
+import mongoengine as db
 import datetime
 import pytz
 from pytz import timezone
 
-class User(Document):
+class User(db.Document):
     name = StringField()
     surname=StringField()
     username = StringField(Required=True,unique=True)
     password = StringField(Required=True)
     email = EmailField()
     
-    utc_now = datetime.datetime.utcnow()
-    utc = pytz.timezone('UTC')
-    aware_date = utc.localize(utc_now)
-    turkey = timezone('Europe/Istanbul')
-    date_modified = DateTimeField(default=aware_date.astimezone(turkey))
+    def to_json(self):
+             return{
+             "name": self.name,
+             "surname": self.surname,
+             "username": self.username,
+             "password":self.password,
+             "email": self.email
+         }
+    # utc_now = datetime.datetime.utcnow()
+    # utc = pytz.timezone('UTC')
+    # aware_date = utc.localize(utc_now)
+    # turkey = timezone('Europe/Istanbul')
+    # date_modified = DateTimeField(default=aware_date.astimezone(turkey))
 
 
-class Blog(Document):
+class Blog(db.Document):
     user_id = IntField(Required=True)
     title = StringField(Required=True)
     author = StringField(Required=True)
     description = StringField(Required=True)
+    
+    def to_json(self):
+            return{
+            "user_id": self.user_id,
+            "author": self.author,
+            "title": self.title,
+            "description": self.description
+        }
 
     
--- src/backend/main.py
+++ src/backend/main.py
@@ -6,64 +6,95 @@ database_name = "interntask"
 password =mongo_password
 DB_URL="mongodb+srv://duzgunberke:10.s0Bi0@pythoncluster.g4lwsqz.mongodb.net/{}?retryWrites=true&w=majority".format(database_name)
 db.connect(host=DB_URL)
+from classes import User,Blog 
 
 
-class Blog(db.Document):
-    blog_id=db.IntField()
-    name=db.StringField()
-    author=db.StringField()
+# class Blog(db.Document):
+#     user_id=db.ObjectIdField(required=True)
+#     author=db.StringField(required=True)
+#     title = db.StringField(Required=True)
+#     description = db.StringField(Required=True)
     
-    def to_json(self):
-        return{
-            "blog_id": self.blog_id,
-            "name": self.name,
-            "author": self.author
-        }
+#     def to_json(self):
+#         return{
+#             "user_id": self.user_id,
+#             "author": self.author,
+#             "title": self.title,
+#             "description": self.description
+#         }
 
-print("\n Create a blog")
-blog=Blog(blog_id=1,
-          name="Deneme",
-          author="Berke"
-        )
-blog.save()
+# print("\n Create a blog")
+# blog=Blog(
+#           user_id="637a99320a792735f42e026e",
+#           author="Berke",
+#           title="Deneme",
+#           description="Lorem ipis rem ipis rem "
+#         )
+# blog.save()
 
+user=User(name="Efe",surname="Dzgn",username="dzgnefe",password="123456",email="duzgunberke2@gmail.com")
+user.save()
 
-print("\nFetch a blog")
-blog=Blog.objects(blog_id=1).first()
+# print("\nFetch a blog")
+# blog=Blog.objects(blog_id=1).first()
+# # print(blog.to_json())
+# print("\nUpdate a blog")
+# blog.update(name="Deneme 2",
+#             author="Efe")
 # print(blog.to_json())
-print("\nUpdate a blog")
-blog.update(name="Deneme 2",
-            author="Efe")
-print(blog.to_json())
 
-print("\n Create a another blog")
-blog=Blog(blog_id=2,
-          name="Deneme 3",
-          author="Aslı"
-        )
-blog.save()
+# print("\n Create a another blog")
+# blog=Blog(blog_id=2,
+#           name="Deneme 3",
+#           author="Aslı"
+#         )
+# blog.save()
 
-print("\n Create a another blog")
-blog=Blog(blog_id=3,
-          name="Selam Deneme",
-          author="Aslı"
-        )
-blog.save()
+# print("\n Create a another blog")
+# blog=Blog(blog_id=3,
+#           name="Selam Deneme",
+#           author="Aslı"
+#         )
+# blog.save()
 
-print("\n Fetch all blogs")
-blogs=[]
-for blog in Blog.objects():
-    blogs.append(blog.to_json())
-print(blogs)    
+# print("\n Fetch all blogs")
+# blogs=[]
+# for blog in Blog.objects():
+#     blogs.append(blog.to_json())
+# print(blogs)    
 
-print("\n Find blog whose name contains Selam")
-for blog in Blog.objects(name__contains="Selam"):
-    blogs.append(blog.to_json())
+# print("\n Find blog whose name contains Selam")
+# for blog in Blog.objects(name__contains="Selam"):
+#     blogs.append(blog.to_json())
 
 
-print("\nDelete a blog")
-blog =Blog.objects(blog_id=2).first()
-blog.delete()
+# print("\nDelete a blog")
+# blog =Blog.objects(blog_id=2).first()
+# blog.delete()
+
+# class User(db.Document):
+#     name = db.StringField()
+#     surname=db.StringField()
+#     username = db.StringField(Required=True,unique=True)
+#     password = db.StringField(Required=True)
+#     email = db.EmailField()
+    
+#     def to_json(self):
+#         return{
+#             "name": self.name,
+#             "surname": self.surname,
+#             "username": self.username,
+#             "password":self.password,
+#             "email": self.email
+#         }
+
+# user=User(
+#           name="Berke",
+#           surname="Duzgun",
+#           username="duzgunberke",
+#           password="123456",
+#           email="duzgunberke@gmail.com")
+# user.save()
 
 # app= Flask(__name__)
 
