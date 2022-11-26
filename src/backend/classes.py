d2374d55c2f816984687da005571f5af19d62b27
duzgunberke <79326338+duzgunberke@users.noreply.github.com>
2022-11-26 21:07:06 +0300

    pre complate the api side

--- src/backend/classes.py
+++ src/backend/classes.py
@@ -4,40 +4,27 @@ import datetime
 import pytz
 from pytz import timezone
 
-class User(db.Document):
+class User(Document):
     name = StringField()
     surname=StringField()
     username = StringField(Required=True,unique=True)
     password = StringField(Required=True)
     email = EmailField()
+    author = BooleanField(default=False)
+    utc_now = datetime.datetime.utcnow()
+    utc = pytz.timezone('UTC')
+    aware_date = utc.localize(utc_now)
+    turkey = timezone('Europe/Istanbul')
+    date_modified = DateTimeField(default=aware_date.astimezone(turkey))
     
-    def to_json(self):
-             return{
-             "name": self.name,
-             "surname": self.surname,
-             "username": self.username,
-             "password":self.password,
-             "email": self.email
-         }
-    # utc_now = datetime.datetime.utcnow()
-    # utc = pytz.timezone('UTC')
-    # aware_date = utc.localize(utc_now)
-    # turkey = timezone('Europe/Istanbul')
-    # date_modified = DateTimeField(default=aware_date.astimezone(turkey))
 
 
-class Blog(db.Document):
+class Blog(Document):
     user_id = ObjectIdField(Required=True)
     title = StringField(Required=True)
     author = StringField(Required=True)
     description = StringField(Required=True)
     
-    def to_json(self):
-            return{
-            "user_id": self.user_id,
-            "author": self.author,
-            "title": self.title,
-            "description": self.description
-        }
+
 
     
--- src/backend/main.py
+++ src/backend/main.py
@@ -1,7 +1,6 @@
 import mongoengine as db
-from api_constant import mongo_password
+
 database_name = "interntask"
-password = mongo_password
 
 from classes import User,Blog 
 from flask import Flask, jsonify,request, Response, make_response
@@ -9,7 +8,7 @@ from flask_cors import CORS
 from bson import ObjectId
 import json
 import jwt
-from datetime import datetime, timedelta
+import datetime
 from flask_bcrypt import Bcrypt
 from functools import wraps
 from pymongo import MongoClient
@@ -58,17 +57,18 @@ for blog in Blog.objects():
     blogs.append(blog.to_json())
 print(blogs) 
 
-
+#region JWT Token
 def tokenReq(f):
     @wraps(f)
     def decorated(*args, **kwargs):
+        
          token = None
 
          if 'token' in request.headers:
             token = request.headers['token']
 
          if not token:
-            return jsonify({'message': 'a valid token is missing'}),401
+            return jsonify({'message': 'a valid token is missing'})
          try:
             data = jwt.decode(token, app.config['SECRET_KEY'], algorithms=[
                 'HS256'])  
@@ -80,51 +80,85 @@ def tokenReq(f):
          return f(current_user, *args, **kwargs)
      
     return decorated
+#endregion
 
 #region Default path
 @app.route('/',methods=['GET'])
 def greetings():
-     return"Hi everyone, this project is working !!!",200
+     return"Hi everyone, this project is working 🚀🚀🚀",200
 #endregion
 
-#region Get All And Insert One Blog
-@app.route('/blogs', methods=['GET', 'POST'])
-def index():
+#region Get All Blogs
+@app.route('/blogs',methods=['GET'])
+def getall():
+    res = []
+    code = 500
+    status = "fail"
+    message = ""
+    try:
+        if(request.method == 'GET'):
+            for blog in Blog.objects():  
+             res.append(blog.to_json())
+                 
+            if res:
+             message = "blogs retrieved"
+             status = 'successful'
+             code = 200
+            else:
+             message = "no blogs found"
+             status = 'successful'
+             code = 200
+        else:
+            return "This method is not correct !"     
+    except Exception as ee:
+        res = {"error": str(ee)}
+    return jsonify({"status":status,'data': res, "message":message}), code    
+#endregion
+
+#region Get Blog By ID   // Burada body kısmından ID gondermesi yapıyor header kısmından olmalı
+@app.route('/getblogbyid', methods=["POST"])
+def getBlogById():
+    if request.method == "POST":
+        id = request.json['id']
+        blog = Blog.objects.get(id=id)
+
+        return make_response(jsonify(blog.to_json()))
+#endregion
+
+#region Insert One Blog
+@app.route('/addblog', methods=['POST'])
+@tokenReq
+def addblog(current_user):
     res = []
     code = 500
     status = "fail"
     message = ""
     try:
         if (request.method == 'POST'):
-            res = db['interntask.blog'].insert_one(request.get_json())
-            if res.acknowledged:
+            title = request.json['title']
+            author = current_user["username"]
+            description = request.json['description']
+            blog=Blog(title=title, author=author,description=description)
+            blog.save()
+            
+            if  blog:
                 message = "item saved"
                 status = 'successful'
                 code = 201
-                res = {"_id": f"{res.inserted_id}"}
+                res = "{}".format(blog.title)
             else:
                 message = "insert error"
                 res = 'fail'
                 code = 500
         else:
-             blogs=[]
-             for blog in Blog.objects():
-                 blogs.append(blog.to_json())   
-             if res:
-                 message = "blogs retrieved"
-                 status = 'successful'
-                 code = 200
-             else:
-                 message = "no blogs found"
-                 status = 'successful'
-                 code = 200
+             return "This method is not correct !"
     except Exception as ee:
         res = {"error": str(ee)}
     return jsonify({"status":status,'data': res, "message":message}), code
 #endregion
 
-#region Get One and update one
-@app.route('/getblog/<item_id>', methods=['GET', 'POST'])
+#region Update One Blog
+@app.route('/editblog/<item_id>', methods=['GET', 'POST'])
 @tokenReq
 def by_id(item_id):
     data = {}
@@ -160,7 +194,6 @@ def by_id(item_id):
     return jsonify({"status": status, "message":message,'data': data}), code
 #endregion
 
-
 #region Signup
 @app.route('/signup', methods=['POST'])
 def save_user():
@@ -191,9 +224,10 @@ def login():
         password = request.json['password']  # password
         user = User.objects.get(username=username)
 
-        if bcrypt.check_password_hash(user.password, user["password"]):
+        if bcrypt.check_password_hash(user["password"], password):
                 token = jwt.encode({'username': username, 'exp': datetime.datetime.utcnow(
                 ) + datetime.timedelta(minutes=15)}, app.config['SECRET_KEY'])
+                token=token.decode('utf8')
                 return jsonify({'token': token})
 
         else:
@@ -201,9 +235,23 @@ def login():
 
 #endregion            
 
+#region Logout
+@app.route('/logout', methods=['POST'])
+@tokenReq
+def logout(current_user):
+    return jsonify({"message": True})
+#endregion
 
+#region Get Users
+@app.route('/users', methods=["GET"])
+def user():
 
-
+    users = []
+    for user in User.objects():  
+        users.append(user.to_json())
+    # json_users=json.dumps(users)
+    return make_response(jsonify(users)) 
+#endregion
 
 
 
