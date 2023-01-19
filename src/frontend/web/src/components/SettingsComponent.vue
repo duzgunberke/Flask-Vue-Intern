46a711847ad694cabc25b27ff902de35fb162a71
duzgunberke <79326338+duzgunberke@users.noreply.github.com>
2023-01-20 02:47:06 +0300

    v-if-check-settings

--- src/frontend/web/src/components/SettingsComponent.vue
+++ src/frontend/web/src/components/SettingsComponent.vue
@@ -1,30 +1,35 @@
 <template>
-      <form @submit.prevent="updateUser">
-        <div class="form-outline mb-4">
-            <label class="form-label" >Username : </label>
-            <p class="form-label bg-white">{{user.username}}</p>
-        </div>
-    
-        <div class="form-outline mb-4">
-            <label class="form-label">First Name :</label>
-            <p class="form-label bg-white">{{user.name}}</p>
-            <input class="form-control" required v-model="name" />
-        </div>
-    
-        <div class="form-outline mb-4">
-            <label class="form-label">Surname :</label>
-            <p class="form-label bg-white">{{user.surname}}</p>
-            <input class="form-control" required v-model="surname" />
-        </div>
-    
-        <div class="form-outline mb-4">
-            <label class="form-label">Email :</label>
-            <p class="form-label bg-white">{{user.email}}</p>
-            <input class="form-control" required v-model="email" />
-        </div>
-    
-         <button class="btn btn-primary btn-block mb-4">Update</button>
-    </form>
+    <div v-if="user">
+        <form @submit.prevent="updateUser">
+          <div class="form-outline mb-4">
+              <label class="form-label" >Username : </label>
+              <p class="form-label bg-white">{{user.username}}</p>
+          </div>
+      
+          <div class="form-outline mb-4">
+              <label class="form-label">First Name :</label>
+              <p class="form-label bg-white">{{user.name}}</p>
+              <input class="form-control" required v-model="name" />
+          </div>
+      
+          <div class="form-outline mb-4">
+              <label class="form-label">Surname :</label>
+              <p class="form-label bg-white">{{user.surname}}</p>
+              <input class="form-control" required v-model="surname" />
+          </div>
+      
+          <div class="form-outline mb-4">
+              <label class="form-label">Email :</label>
+              <p class="form-label bg-white">{{user.email}}</p>
+              <input class="form-control" required v-model="email" />
+          </div>
+      
+           <button class="btn btn-primary btn-block mb-4">Update</button>
+      </form>
+    </div>
+    <div v-else>
+        <h1>You are not author ! </h1>
+    </div>
 </template>
 
 <script>
